fd9edb5583a24ccbb2fb2cddec0c49c013d2a497
Akshay Dodeja <dodeja@gmail.com>
2010-01-07 18:23:36 -0800

    deploy update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,35 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
-
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+default_run_options[:pty] = true
+ 
+set :application, "aboutme"
+set :user, "dodeja"
+set :repository,  "git@github.com:dodeja/aboutme.git"
+set :port, 30007
+ 
+set :runner, "dodeja"
+set :use_sudo, false
+ 
+set :deploy_to, "/home/dodeja/www/#{application}"
+ 
+ 
+set :scm, "git"
+set :scm_passphrase, "a317dmx" 
+ 
+set :branch, "master"
+ 
+ set :shared_host, "204.232.206.167"
+set :location, "204.232.206.167"
+ 
+role :app, location
+role :web, location
+role :db,  location, :primary => true
+ 
+ 
+ 
+namespace :deploy do
+  desc "Tell Passenger to restart the app."
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
